b26ef8bb15c82e616364c4b1ee5e5d595a6aa200
Hap29 <81244257+Hap29@users.noreply.github.com>
2021-03-23 16:37:00 +0100

    Create SQL-ENVOI-PYTHON.py

---
+++ BDD_ET_COMMANDES/SQL-ENVOI-PYTHON.py
@@ -0,0 +1,24 @@
+import mysql.connector
+import datetime
+import time
+import random
+
+#db = mysql.connector.connect(host="192.168.0.201",user="admin",password="mot_de_passe", database="r2energie")    # @IP_LYCEE  
+db = mysql.connector.connect(host="localhost",user="admin",password="mot_de_passe", database="r2energie")      # @IP_MAISON
+
+cur = db.cursor()
+sql = "INSERT INTO DATAS_DEVICE (HUMIDITEAIR, TEMPERATUREAIR,TEMPERATURESOL,HUMIDITESOL, ENSOLEILLEMENT,DATE,HEURE ) VALUES (%s, %s,%s, %s,%s,%s,%s)"
+
+i = 0
+while(i< 30) :
+    date = str(datetime.date.today())
+    heure = str(datetime.datetime.now().strftime("%H:%M:%S"))
+    value = (random.randint(55,60), random.randint(20, 22),random.randint(20, 22), random.randint(55,60), random.randint(500, 800),  date, heure )
+    i += 1
+    cur.execute(sql,value) 
+    db.commit()
+    time.sleep(0.1)
+
+    
+
+
